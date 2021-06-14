4cfd7dcdd89ab000a5fabb35a215106765ab8019
Cloud User <centos@ip-172-31-1-101.eu-west-2.compute.internal>
2021-06-14 14:04:04 +0000

    working code

--- Ex4/top.v
+++ Ex4/top.v
@@ -15,4 +15,22 @@
 //
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
+module top(clk,rst,button,colour);
+	input clk;
+	input rst;
+	input button;
+	output reg [2:0] colour;
 
+	always @(posedge clk or posedge rst)
+		begin 
+		if (rst || ^colour==3'bx)
+			colour<=3'b000;    /// assume goes to 0 at reset 
+		else 
+			case(colour)
+				3'b000: colour<=3'b001;
+				3'b111: colour<=3'b001;
+				3'b110: colour<=colour-(3'b101)*(button);
+				default: colour<=colour+(3'b001)*(button);
+			endcase
+		end
+endmodule 
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -1,9 +1,48 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Test bench for Exercise #4 - Dynamic LED lights
-// Student Name:
-// Date: 
+// Student Name: Haris Saeed
+// Date: 14/06/21
 //
 // Description: A testbench module to test Ex4 - Dynamic LED lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+module testbench;
+	parameter [2:0] initial_colour=3'b000;
+	
+	reg bench_clk=1'b0;
+	reg bench_rst=1'b0;
+	reg bench_button=1'b0;
+	wire [2:0]bench_colour;
+    
+    always 
+	begin
+		#2 bench_clk<=~bench_clk;
+		$monitor($time, " clk = %b , rst = %b , button = %b , colour = %b  ",
+		bench_clk, bench_rst, bench_button, bench_colour);
+		#2 bench_button<=~bench_button;
+		bench_clk<=~bench_clk;
+		$monitor($time, " clk = %b , rst = %b , button = %b , colour = %b  ",
+		bench_clk, bench_rst, bench_button, bench_colour);
+	end 
 
+	top inst(
+        .clk(bench_clk), 
+        .rst(bench_rst), 
+        .button(bench_button), 
+        .colour(bench_colour)
+    );
+    
+    
+	initial
+	begin  
+		bench_rst<=1'b1;
+		#2 bench_rst<=1'b0;
+		#54 bench_rst<=1'b1;
+		#2 bench_rst<=1'b0 ;
+	end
+
+	initial
+	begin
+		#100 $finish;
+	end
+endmodule
