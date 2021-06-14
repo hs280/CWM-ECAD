fd30f1165bf3a7c2c02e6c39a354d16193c62d31
Cloud User <centos@ip-172-31-1-101.eu-west-2.compute.internal>
2021-06-14 10:03:00 +0000

    working one line

--- Ex2/top.v
+++ Ex2/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #2 - Doorbell Chime
-// Student Name:
-// Date: 
+// Student Name: Haris Saeed
+// Date: 14/06/21
 //
 //  Description: In this exercise, you need to design a multiplexer that chooses between two sounds, where the  
 //  output is delayed by 5 ticks (not clocks!) and acts according to the following truth table:
