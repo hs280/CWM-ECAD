9bf07de9c28a1f01424c31d8245feb2d0e87edd3
Cloud User <centos@ip-172-31-1-101.eu-west-2.compute.internal>
2021-06-15 15:29:26 +0000

    seems to work fine(as expected) when accounting for delay

--- Ex6/top_tb.v
+++ Ex6/top_tb.v
@@ -13,20 +13,23 @@ module testbench;
 
 	reg bench_clk=1'b0;
 	reg [2:0]bench_colour=3'b0;
-	reg bench_enable=1'd1;
-	reg [23:0]expected=24'hx;
+	reg bench_enable=1'b0;
+	reg [23:0]expected=24'h0;
 	reg error_flag=1'b0;
-	wire bench_rgb;
+	wire [23:0] bench_rgb;
 
 
 	always
 	begin
 		#5 bench_clk<=~bench_clk;
+        error_flag<=(expected!=bench_rgb)? 1'b1:error_flag;
+        if (error_flag)
+            $display("***TEST FAILED*** ACTUAL=%h , EXPECTED=%h,  COLOUR=%b",bench_rgb,expected,bench_colour); 
     end
 	
-	always begin 
-		#5 bench_enable<=~bench_enable;
-		#5 bench_enable<=~bench_enable;
+	always begin
+		#4 bench_enable<=~bench_enable;
+		#6 bench_enable<=~bench_enable;
 		#10;
 	end
 		
@@ -38,28 +41,29 @@ module testbench;
     );
 
 	initial 
-	   
-    		#5;
-			always@ (posedge bench_clk  && bench_enable)  begin
-				case(bench_colour)
-					3'b000: expected<=24'h000000;
-					3'b001: expected<=24'h0000FF;
-					3'b010: expected<=24'h00FF00;
-					3'b011: expected<=24'h00FFFF;
-					3'b100: expected<=24'hFF0000;
-					3'b101: expected<=24'hFF00FF;
-					3'b110: expected<=24'hFFFF00;
-					3'b111: expected<=24'hFFFFFF;
-					default:  expected<=24'hx;		
+        #10;
+			always@ (posedge bench_clk)  begin
+				if (bench_enable) begin 
+				    case(bench_colour)  //propagation delay of ipa
+					3'b000: #20 expected<=24'h000000;
+					3'b001: #20 expected<=24'h0000FF;
+					3'b010: #20 expected<=24'h00FF00;
+					3'b011: #20 expected<=24'h00FFFF;
+					3'b100: #20 expected<=24'hFF0000;
+					3'b101: #20 expected<=24'hFF00FF;
+					3'b110: #20 expected<=24'hFFFF00;
+					3'b111: #20 expected<=24'hFFFFFF;
+					default:#20 expected<=24'hx;		
                 endcase 
-            end    	    
+                end 
+                else 
+                expected<=expected;
+           end    	    
 initial 
-    #2;
+    #5;
     always 
 		  begin
-			#5 bench_colour<=bench_colour+3'b1;
-			error_flag<=(expected!=bench_rgb)? 1'b1:error_flag;
-			$display("***TEST FAILED*** ACTUAL=%h , EXPECTED=%h,  COLOUR=%b",bench_rgb,expected,bench_colour);			
+			#12 bench_colour<=bench_colour+3'b1;			
         end
     
 	initial
