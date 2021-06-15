79c1aef99db615722313731a5f90de700bddf66d
Cloud User <centos@ip-172-31-1-101.eu-west-2.compute.internal>
2021-06-15 08:44:25 +0000

    workinhg tb

--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -6,14 +6,21 @@
 // Description: A testbench module to test Ex4 - Dynamic LED lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
+
+
+
+
+
 module testbench;
-	parameter [2:0] initial_colour=3'b000;
-	
 	reg bench_clk=1'b0;
 	reg bench_rst=1'b0;
 	reg bench_button=1'b0;
+	reg [2:0]expected_colour;
+	reg error_flag=1'b0;
 	wire [2:0]bench_colour;
-    
+
+
     always 
 	begin
 		#2 bench_clk<=~bench_clk;
@@ -24,25 +31,48 @@ module testbench;
 		$monitor($time, " clk = %b , rst = %b , button = %b , colour = %b  ",
 		bench_clk, bench_rst, bench_button, bench_colour);
 	end 
-
 	top inst(
         .clk(bench_clk), 
         .rst(bench_rst), 
         .button(bench_button), 
         .colour(bench_colour)
     );
-    
-    
-	initial
-	begin  
-		bench_rst<=1'b1;
-		#2 bench_rst<=1'b0;
-		#54 bench_rst<=1'b1;
-		#2 bench_rst<=1'b0 ;
+
+	always 
+		begin 
+		#1
+		if (expected_colour!=bench_colour)  begin 
+	       		error_flag<=1;
+			$display("**TEST FAILED**  actual %b, expected %b", bench_colour, expected_colour); end
+	     else  begin 
+	        	error_flag<=error_flag; end 		
+
 	end
 
 	initial
-	begin
-		#100 $finish;
+	begin  
+		#1 bench_rst<=1'b1;
+		expected_colour<=3'b1;
+		#2 bench_rst<=1'b0;
+		#54 bench_rst<=1'b1;
+		expected_colour<=3'b1;
+		#2 bench_rst<=1'b0 ;
 	end
+
+initial 
+    #2;
+        always @(posedge bench_clk && bench_button) begin
+            expected_colour<=(expected_colour==3'b110)? 3'b1: expected_colour+3'b001;       
+    end            
+	
+	initial
+	begin
+	   #150;
+	   if (!error_flag)
+	       $display("***TEST PASSED***");
+       else 
+        $display("***TEST FAILED***");
+		$finish;
+	end
+
 endmodule
