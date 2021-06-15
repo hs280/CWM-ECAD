f5898948cf0c8c413969e6e0ead2cc6e7cf9dbe7
Cloud User <centos@ip-172-31-1-101.eu-west-2.compute.internal>
2021-06-15 10:13:47 +0000

    WORKING TB

--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -7,13 +7,13 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
-module testbench;
 
-	parameter heat_ratio=5'd2;	// modify paramater for testing purposes
-    	parameter natural_temperature=5'd17;
+module testbench;
 
 	reg bench_clk=1'b0;
 	reg [4:0]bench_temperature=5'd5;
+	reg [1:0]expected=2'bx;
+	reg error_flag=1'b0;
 	wire bench_heating;
 	wire bench_cooling;
 
@@ -27,10 +27,7 @@ module testbench;
 			else if (bench_cooling) begin 
 				bench_temperature<=bench_temperature-5; end
 			else	begin		
-			     if (bench_temperature<natural_temperature) begin 
-				bench_temperature<=(bench_temperature<10)? bench_temperature+5'd1:bench_temperature+5'd1; end
-				else begin 
-			bench_temperature<=bench_temperature-1;end end 
+			bench_temperature<=bench_temperature-1;end  
 		#2 bench_clk=~bench_clk;
     end
 
@@ -47,9 +44,30 @@ module testbench;
 			bench_clk, bench_temperature, bench_heating, bench_cooling);
 	end
 
+	initial 
+    	#2;
+        always @(posedge bench_clk) begin
+		if (bench_heating) 
+			expected<=(bench_temperature<5'd20)? 2'b10: 2'b00;
+		else if (bench_cooling)  
+			expected<=(bench_temperature>5'd20)? 2'b01:2'b00;
+		else 
+			expected<=(bench_temperature>=22)? 2'b01:(bench_temperature<=19)? 2'b10:2'b00;
 
+		if (expected!={bench_heating,bench_cooling}) begin
+			error_flag<=1'b1;
+			$display("***TEST FAILED***  ACTUAL= %b, EXPECTED+ %b",{bench_cooling,bench_heating},expected); end
+		else error_flag<=error_flag;
+    	end   
+    
+    
 	initial
 	begin
-		#500 $finish;
+		#500 
+		if (error_flag)
+			$display("***TEST FAILED***");
+		else 
+			$display("***TEST PASSED***");
+		$finish;
 	end
 endmodule
